6e2badca80efb56a4dd754d611544a9ba5687084
Louis VIVIER <github@louisvivier.fr>
2021-01-21 15:51:19 +0100

    Init AS project

---
+++ src/app/src/test/java/com/junia/isen/m1_devsec_sa/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.junia.isen.m1_devsec_sa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
